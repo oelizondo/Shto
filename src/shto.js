2756393b5f3f65091920d23717285a1238f89be3
oelizondo <oscarmarcelo95@gmail.com>
2016-04-12 22:46:06 -0500

    adds some mothods to api

--- src/shto.js
+++ src/shto.js
@@ -1,15 +1,33 @@
 class Shto {
-  constructor(trigger, action) {
-    this.trigger = trigger
-    this.action  = action
+  constructor(trigger, action, domElements) {
+    this.trigger     = trigger
+    this.action      = action
+    this.domElements = domElements
   }
 
   listen () {
-    if (typeof window.sessionStorage == 'undefined') {
+    if (typeof window.sessionStorage == 'undefined' || typeof window.localStorage == 'undefined') {
       throw 'sessionStorage is not enabled / supported'
     }
     else {
-      console.log('listening')
+      const trigger = document.findElementById(this.trigger)
+      trigger.addEventListener(this.action, captureData)
+
     }
   }
+
+  addValue(element) {
+    if (typeof element == 'string') this.domElements.push(element)
+    return true;
+  }
+
+  captureData() {
+    this.domElements.forEach(function(domElement){
+      window.sessionStorage.setItem(domElement, document.getElementById(domElement).value)
+    })
+  }
+
+
 }
+
+const sess = new Shto('save', 'click', ['button1', 'button2', 'button3']);
