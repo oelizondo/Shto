4ca486456edebe29a0be4a400894d8cfbd4b060f
oelizondo <oscarmarcelo95@gmail.com>
2016-04-17 22:14:44 -0500

    fixes ternary operator bug

--- src/shto.js
+++ src/shto.js
@@ -1,46 +1,45 @@
 class Shto {
   constructor(action, trigger, domElements) {
-    this.trigger     = trigger
     this.action      = action
+    this.trigger     = trigger
     this.domElements = domElements
   }
 
   listen () {
     if (typeof window.sessionStorage != "undefined") {
-
       const trigger = document.getElementById(this.trigger)
       trigger.addEventListener(this.action, () => { this.captureData(this.domElements) })
-
     } else {
-
       throw "sessionStorage is not enabled / supported."
-
     }
-      
     window.onload = this.returnData(this.domElements)
   }
 
   addValue(element) {
-    typeof element == "string" ? this.domElements.push(element) : throw 'Not a valid element.'
+    if (typeof element == "string") {
+      this.domElements.push(element)
+    } else { 
+      throw 'Not a valid element.'
+    }
   }
 
   captureData(domElements) {
     domElements.forEach((domElement) => {
-      domElement.value != "undefined" ? window.sessionStorage.setItem(domElement, document.getElementById(domElement).value) : throw "This element doesn't exist."
+      if (domElement.value != "undefined") { 
+        window.sessionStorage.setItem(domElement, document.getElementById(domElement).value)
+      } else { 
+        throw "This element doesn't exist." 
+      }
     })
   }
 
   returnData(domElements) {
     domElements.forEach((domElement) => {
       if (domElement.value != "undefined") {
-
         const formField = document.getElementById(domElement)
         formField.value = window.sessionStorage.getItem(domElement)
-
       } else {
-
         throw "This element doesn't exist."
-
       }
     })
   }
